89604d4d84c986b7f8180b28070bec2849e985c3
Jhon Jimenez <jejimenez1908@gmail.com>
2026-03-13 16:10:46 -0500

    Estructura final: inventario-app con DTOs y Paginación

---
+++ inventario-app/src/main/java/com/jejimenez/EquipoDTO.java
@@ -0,0 +1,9 @@
+package com.jejimenez;
+
+public class EquipoDTO {
+    public Long id;
+    public String marca;
+    public String modelo;
+    public String nombreCategoria; // enviamos el texto, no el objeto entero
+
+}
